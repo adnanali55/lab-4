42e8d5df666e5306428af7a3d4fa2c3b5bd7a73f
adnanali55 <f2025-0968@bnu.edu.pk>
2025-10-24 14:42:53 +0500

    Add Task 1.cpp with basic output information

---
+++ Task 1.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main() {
+    cout<<"Name:\tSheeza Batool\n";
+    cout<<"Department:\tScit\n";
+    cout<<"Quote:\tKnowledge is Power\t\n";
+
+    return 0;
+
+}
